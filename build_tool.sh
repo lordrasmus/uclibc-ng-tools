6e6169dc95faa55d61f76c07781dc6faaa94a199
ramin <lordrasmus@gmail.com>
2023-11-21 22:21:27 +0100

    build_tool.sh

--- build_tool.sh
+++ build_tool.sh
@@ -28,6 +28,11 @@ done
 KERNEL_VERS=6.5.10
 if [[ $CONFIG_KERNEL_ARCH == "cris" ]] ; then KERNEL_VERS=4.9.156; fi
 if [[ $CONFIG_KERNEL_ARCH == "m68k" ]] ; then KERNEL_VERS=6.1.62; fi
+if [[ $CONFIG_KERNEL_ARCH == "avr32" ]] ; then KERNEL_VERS=4.4.302; fi
+if [[ $CONFIG_KERNEL_ARCH == "kvx" ]] ; then KERNEL_VERS=kvx; fi
+
+#if [[ $CONFIG_KERNEL_ARCH == "xtensa" ]] ; then KERNEL_VERS=6.1.26; fi
+
 #if [[ $CONFIG_KERNEL_ARCH == "m68k" ]] ; then KERNEL_VERS=6.3.9; fi
 
 
@@ -51,6 +56,8 @@ prepare_kernel(){
 
 conf_kernel(){
 	
+	install_toolchain
+	
 	prepare_kernel
 	
 	print_status "conf_kernel"
@@ -60,6 +67,8 @@ conf_kernel(){
 
 
 build_kernel(){
+
+	install_toolchain
 
 	prepare_kernel
 	
@@ -82,18 +91,21 @@ build_kernel(){
 	if [[ $CONFIG_KERNEL_ARCH == "openrisc" ]] ;  then ${CONFIG_GCC_PREFIX}objcopy  -S linux-$KERNEL_VERS/vmlinux kernel.img ; fi
 	if [[ $CONFIG_KERNEL_ARCH == "m68k" ]] ;      then ${CONFIG_GCC_PREFIX}objcopy  -S linux-$KERNEL_VERS/vmlinux kernel.img ; fi
 	if [[ $CONFIG_KERNEL_ARCH == "mips" ]] ;      then ${CONFIG_GCC_PREFIX}objcopy  -S linux-$KERNEL_VERS/vmlinux kernel.img ; fi
+	if [[ $CONFIG_KERNEL_ARCH == "mips64" ]] ;    then ${CONFIG_GCC_PREFIX}objcopy  -S linux-$KERNEL_VERS/vmlinux kernel.img ; fi
 	if [[ $CONFIG_KERNEL_ARCH == "nios2" ]] ;     then ${CONFIG_GCC_PREFIX}objcopy  -S linux-$KERNEL_VERS/vmlinux kernel.img ; fi
+	if [[ $CONFIG_KERNEL_ARCH == "kvx" ]] ;       then ${CONFIG_GCC_PREFIX}objcopy  -S linux-$KERNEL_VERS/vmlinux kernel.img ; fi
 	
-	if [[ $CONFIG_KERNEL_ARCH == "sh" ]] ;     then cp linux-$KERNEL_VERS/arch/sh/boot/zImage kernel.img ; fi
-	if [[ $CONFIG_KERNEL_ARCH == "sparc" ]] ;     then cp linux-$KERNEL_VERS/arch/sparc/boot/zImage kernel.img ; fi
-	
-	if [[ $CONFIG_KERNEL_ARCH == "x86" ]] ;     then cp linux-$KERNEL_VERS/arch/x86/boot/bzImage kernel.img ; fi
-	
-	if [[ $CONFIG_KERNEL_ARCH == "xtensa" ]] ;     then cp linux-$KERNEL_VERS/arch/xtensa/boot/Image.elf kernel.img ; fi
+	if [[ $CONFIG_KERNEL_ARCH == "sh" ]] ;        then cp linux-$KERNEL_VERS/arch/sh/boot/zImage kernel.img        ; fi
+	if [[ $CONFIG_KERNEL_ARCH == "sparc" ]] ;     then cp linux-$KERNEL_VERS/arch/sparc/boot/zImage kernel.img     ; fi
+	if [[ $CONFIG_KERNEL_ARCH == "x86" ]] ;       then cp linux-$KERNEL_VERS/arch/x86/boot/bzImage kernel.img      ; fi
+	if [[ $CONFIG_KERNEL_ARCH == "xtensa" ]] ;    then cp linux-$KERNEL_VERS/arch/xtensa/boot/Image.elf kernel.img ; fi
+	if [[ $CONFIG_KERNEL_ARCH == "riscv" ]] ;     then cp linux-$KERNEL_VERS/arch/riscv/boot/Image kernel.img      ; fi
 
 }
 
 para_kernel(){
+	
+	install_toolchain
 	
 	prepare_kernel
 	
@@ -103,6 +115,8 @@ para_kernel(){
 
 
 oldconf_kernel(){
+	
+	install_toolchain
 	
 	prepare_kernel
 
@@ -118,23 +132,52 @@ upload_kernel(){
 		git clone git@github.com:lordrasmus/uclibc-ng-qemu-imgs.git
 	fi
 	
+	
+	
 	if [ $# -ne 1 ] ; then
-		echo "kernel name angeben"
+	
+		if [[ $CONFIG_QEMU_KERNEL == "" ]] ; then
+			echo "kernel name angeben"
+			exit 1
+		fi
+		
+		CONFIG_QEMU_KERNEL=${CONFIG_QEMU_KERNEL%%.*}
+		
+		kernel_name=$CONFIG_QEMU_KERNEL
+	else
+		kernel_name=$1
+	fi
+	
+	
+	echo "Kernel Name : $kernel_name"
+	
+	
+	read -p "Möchtest du fortfahren? (j/n): " antwort
+	
+	if [ "$antwort" == "j" ] || [ "$antwort" == "J" ]; then
+		echo "Du hast mit Ja geantwortet. Fortsetzen..."
+		# Hier kannst du den Code platzieren, der bei einer positiven Antwort ausgeführt werden soll.
+	else
+		echo "Du hast mit Nein geantwortet. Abbruch."
 		exit 1
 	fi
 	
-	echo "Kernel Name : $1"
-	
-	cp kernel.img uclibc-ng-qemu-imgs/$1.img
-	cp linux-$KERNEL_VERS/.config uclibc-ng-qemu-imgs/$1.config
+	cp kernel.img uclibc-ng-qemu-imgs/$kernel_name.img
+	cp linux-$KERNEL_VERS/.config uclibc-ng-qemu-imgs/$kernel_name.config
 	
 	( cd uclibc-ng-qemu-imgs; git status )
+	( cd uclibc-ng-qemu-imgs; git add * )
+	( cd uclibc-ng-qemu-imgs; git commit -m "kernel_name" )
+	( cd uclibc-ng-qemu-imgs; git push )
 	
 	
 }
 
 
 fill_sysroot(){
+	
+	install_toolchain
+	
 	if [ ! -e sysroot ] ; then
 		tar -xf sysroot.tar
 	fi
@@ -149,6 +192,8 @@ fill_sysroot(){
 }
 
 build_rootfs(){
+
+	install_toolchain
 
 	fill_sysroot
 
@@ -183,22 +228,23 @@ build_rootfs(){
 	cat uclibc-ng-config | grep SHAR
 	
 	rm -f tests_disable
-	echo "" > tests_disable
-	if grep -q '# UCLIBC_HAS_LOCALE is not set' uclibc-ng-config; then echo "export NO_LOCALE=1 " >> tests_disable; fi
-	if grep -q 'HAS_NO_THREADS=y' uclibc-ng-config;               then echo "export NO_THREADS=1  NO_NPTL=1  NO_TLS=1 "  >> tests_disable ; fi
-	if grep -q 'CONFIG_SH2=y' uclibc-ng-config;                   then echo "export NO_MATH=1" >> tests_disable; fi
-	if grep -q '# UCLIBC_HAS_OBSOLETE_BSD_SIGNAL is not set' uclibc-ng-config;  then echo "export  NO_NPTL=1 "  >> tests_disable ; fi
-	if grep -q 'ARCH_HAS_NO_SHARED=y' uclibc-ng-config;           then echo "export NO_DL=1 "  >> tests_disable ; fi
-	
-	if grep -q 'TARGET_alpha=y' uclibc-ng-config;                 then echo "export NO_MATH=1" >> tests_disable; fi
-	if grep -q 'TARGET_sparc=y' uclibc-ng-config;                 then echo "export NO_MISC=1 NO_NPTL=1 " >> tests_disable; fi
-	
+	echo "export $(./uclibc-ng-test/get_disabled_tests.py uclibc-ng-config)" > tests_disable
+
 	
 
 	. tests_disable
 	CROSS_COMPILE=$CONFIG_GCC_PREFIX CFLAGS="--sysroot=$(pwd)/sysroot/" LDFLAGS="--sysroot=$(pwd)/sysroot/ "   make -C uclibc-ng-test -j20 #V=1
 	CROSS_COMPILE=$CONFIG_GCC_PREFIX CFLAGS="--sysroot=$(pwd)/sysroot/" LDFLAGS="--sysroot=$(pwd)/sysroot/ "   make -C uclibc-ng-test install DESTDIR=../rootfs > /dev/null 2>&1
 
+	find rootfs -type f -name '*.gdb' -exec rm {} \;
+	
+	echo "08:00:20:00:61:CA  pal"           > rootfs/etc/ethers
+	echo "08:00:20:00:61:CB  192.168.11.2" >> rootfs/etc/ethers
+	echo "08:00:20:00:61:CC  teeth"        >> rootfs/etc/ethers
+	echo "127.0.0.1 localhost"              > rootfs/etc/hosts
+	echo "root:x:0:"                        > rootfs/etc/group
+	echo "root:x:0:0:root:/root:/bin/sh"    > rootfs/etc/passwd
+	
 	#exit 1
 
 	#rm -rf busybox*
@@ -217,6 +263,11 @@ build_rootfs(){
 	sed -i 's|CONFIG_SYSROOT=""|CONFIG_SYSROOT="'$(pwd)'/sysroot/"|' busybox-1.36.1/.config
 	sed -i 's|CONFIG_PREFIX="./_install"|CONFIG_PREFIX="../rootfs"|'  busybox-1.36.1/.config
 	sed -i 's|CONFIG_FEATURE_EJECT_SCSI=y|# CONFIG_FEATURE_EJECT_SCSI is not set|'  busybox-1.36.1/.config
+	sed -i 's|CONFIG_MKSWAP=y|# CONFIG_MKSWAP is not set|'  busybox-1.36.1/.config
+	sed -i 's|CONFIG_SWAPON=y|# CONFIG_SWAPON is not set|'  busybox-1.36.1/.config
+	sed -i 's|CONFIG_SWAPOFF=y|# CONFIG_SWAPOFF is not set|'  busybox-1.36.1/.config
+	
+	
 	
 	
 	# no MMU fixes
@@ -238,11 +289,18 @@ build_rootfs(){
 	#make -C busybox-1.36.1 oldconfig
 
 	#exit 1
-
-	make -C busybox-1.36.1 -j20 
-
-	make -C busybox-1.36.1/ install > /dev/null
-
+	
+	
+	
+	if [[ $UCLIBC_FORMAT_FLAT == "y" ]] ; then
+		echo "no strip"
+		SKIP_STRIP=y make -C busybox-1.36.1 -j20 busybox
+		SKIP_STRIP=y make -C busybox-1.36.1/ install > /dev/null
+	else
+		SKIP_STRIP=0 make -C busybox-1.36.1 -j20 busybox
+		SKIP_STRIP=0 make -C busybox-1.36.1/ install > /dev/null
+	fi
+	
 
 	( cd rootfs ; ln -s /sbin/init init )
 	( cd rootfs ; mkdir dev; mkdir etc; mkdir proc; mkdir sys; mkdir tmp )
@@ -258,12 +316,9 @@ build_rootfs(){
 	#echo "::sysinit:/bin/echo 'test ttyS3' > /dev/ttyS3" >> rootfs/etc/inittab
 	#echo "console::sysinit:/usr/lib/uclibc-ng-test/test/misc/tst-rlimit" >> rootfs/etc/inittab
 	echo "console::sysinit:/bin/run_tests.sh" >> rootfs/etc/inittab
+	#echo "console::respawn:/usr/lib/uclibc-ng-test/test/inet/gethost " >> rootfs/etc/inittab
 	echo "console::respawn:/bin/sh" >> rootfs/etc/inittab
-	echo "08:00:20:00:61:CA  pal" > rootfs/etc/ethers
-	echo "08:00:20:00:61:CB  192.168.11.2" >> rootfs/etc/ethers
-	echo "08:00:20:00:61:CC  teeth" >> rootfs/etc/ethers
-	echo "root:x:0:" > rootfs/etc/group
-	echo "root:x:0:0:root:/root:/bin/sh" > rootfs/etc/passwd
+	
 	
 	echo "#!/bin/sh"                                                           >  rootfs/bin/run_tests.sh
 	echo "cd /usr/lib/uclibc-ng-test/test/"                                    >> rootfs/bin/run_tests.sh
@@ -272,12 +327,155 @@ build_rootfs(){
 	echo "echo '-------------------- tests_end --------------------------'"    >> rootfs/bin/run_tests.sh
 	chmod 777 rootfs/bin/run_tests.sh
     
-    rm -f rootfs.img
-	( cd rootfs ; find . | sort | cpio -o -H newc ) > rootfs.img
-	rm -f rootfs.img.xz
-	xz -k --check=crc32 rootfs.img
+    ( cd rootfs ; ln -s lib lib32 )
+    ( cd rootfs ; ln -s lib lib64 )
+    ( cd rootfs ; ln -s lib libx32 )
+    
+    ( cd rootfs/usr ; ln -s lib lib32 )
+    ( cd rootfs/usr ; ln -s lib lib64 )
+    ( cd rootfs/usr ; ln -s lib libx32 )
+    
+    pack_rootfs
 }
 
+pack_rootfs(){
+    rm -f rootfs.img rootfs.cpio
+	( cd rootfs ; find . | sort | cpio -o -H newc ) > rootfs.img
+	cp rootfs.img rootfs.cpio
+	rm -f rootfs.img.xz
+	xz -k --check=crc32 rootfs.img
+	
+}
+
+
+download_dev_pack(){
+	
+	print_status "download_dev_pack"
+	
+	# URL der Webseite
+	url="https://uclibc-ng.tangotanzen.de/uploads/"
+
+	# Dateiname, den Sie suchen möchten (passen Sie dies an Ihre Anforderungen an)
+	
+
+	# Webseite herunterladen und nach dem Dateinamen suchen
+	filelist=$(curl -s "$url" | grep -oP "(?<=href=\").*?(devel_pack_.*tar)(?=\")" )
+
+	# Überprüfen, ob Dateinamen gefunden wurden
+	if [ -n "$filelist" ]; then
+		echo "Datei(en) gefunden:"
+		# Erstellen Sie ein Array mit den Dateinamen
+		files=($filelist)
+		
+		# Menü anzeigen und Auswahl ermöglichen
+		PS3="Wählen Sie eine Datei zum Herunterladen: "
+		select file in "${files[@]}"; do
+			if [ -n "$file" ]; then
+				# Datei herunterladen
+				dl="${url}/${file}"
+				echo $dl
+				curl -O $dl
+				echo "Die Datei '$file' wurde heruntergeladen."
+				tar -xaf ${file}
+				folder="${file%?}"
+				folder="${folder%?}"
+				folder="${folder%?}"
+				folder="${folder%?}"
+				#echo $folder
+				mv $folder/infos.json .
+				mv $folder/files/* .
+			else
+				echo "Ungültige Auswahl. Bitte erneut versuchen."
+			fi
+			break
+		done
+
+	else
+		echo "Keine Dateien gefunden."
+	fi
+}
+
+run_qemu(){
+	
+	if [[ $CONFIG_KERNEL_ARCH == "kvx" ]] ; then
+		build_kernel
+		../git/uclibc-ng-github/.github/tools/xtensa-cpio-hack.py
+	fi
+	
+	if [[ $CONFIG_KERNEL_ARCH == "xtensa" ]] ; then
+		build_kernel
+		../git/uclibc-ng-github/.github/tools/xtensa-cpio-hack.py
+	fi
+	
+	echo "run : $CONFIG_QEMU_CMD -no-reboot"
+	$CONFIG_QEMU_CMD -no-reboot
+}
+
+
+prepare_uclibc(){
+	
+	install_toolchain
+	
+	print_status "clone uclibc-ng"
+	if [ ! -e uclibc-ng ] ; then
+		git clone git@github.com:lordrasmus/uclibc-ng.git
+	else
+		( cd uclibc-ng; git pull )
+	fi
+	
+	if [ ! -e uclibc-ng/.config ] ; then
+		cp uclibc-ng-config uclibc-ng/.config
+	fi
+	
+	print_status "install kernel_header"
+	if [ ! -e linux-$CONFIG_KERNEL_VERS ] ; then
+		tar -xaf linux-$CONFIG_KERNEL_VERS.tar.xz
+	fi
+	
+	if [ ! -e sysroot/usr/include/linux/version.h ] ; then
+		make -C linux-$CONFIG_KERNEL_VERS/ INSTALL_HDR_PATH=$(pwd)/sysroot/usr/ headers_install ARCH=$CONFIG_KERNEL_ARCH 2>&1 | tee -a build.log
+	fi
+	
+	mkdir -p sysroot/usr/lib/
+	#find $CONFIG_TOOLCHAIN/sysroot
+	if [ -e $CONFIG_TOOLCHAIN/sysroot/usr/lib/ldscripts ] ; then cp -r $CONFIG_TOOLCHAIN/sysroot/usr/lib/ldscripts sysroot/usr/lib/ ; fi
+	if [ -e $CONFIG_TOOLCHAIN/sysroot/usr/lib/libatomic.a ] ; then cp $CONFIG_TOOLCHAIN/sysroot/usr/lib/libatomic* sysroot/usr/lib/ ; fi
+	if [ -e $CONFIG_TOOLCHAIN/sysroot/usr/lib/libgcc_s.so ] ; then cp $CONFIG_TOOLCHAIN/sysroot/usr/lib/libgcc* sysroot/usr/lib/ ; fi
+	#ls sysroot/
+	#( cd sysroot/; ln -s lib lib32; ln -s lib lib64; ln -s lib libx32; )
+	#( cd sysroot/usr; ln -s lib lib32; ln -s lib lib64; ln -s lib libx32; )
+	#if [ -e artifacts/sysroot/lib ] ; then find artifacts/sysroot/lib; fi
+	#if [ -e artifacts/sysroot/usr/lib ] ; then find artifacts/sysroot/usr/lib ; fi
+	#find artifacts/sysroot/
+}
+
+build_uclibc(){
+	
+	install_toolchain
+	
+	prepare_uclibc
+	
+	sed -i 's|KERNEL_HEADERS=.*|KERNEL_HEADERS="../sysroot/usr/include"|g'  uclibc-ng/.config
+	rm -f build.log
+	echo "\n----------------- uClibc-ng log -------------------------- \n" > build.log
+	CROSS_COMPILE=$CONFIG_GCC_PREFIX make -C uclibc-ng oldconfig
+	CROSS_COMPILE=$CONFIG_GCC_PREFIX make -C uclibc-ng 2>&1 | tee -a build.log
+	CROSS_COMPILE=$CONFIG_GCC_PREFIX make -C uclibc-ng install DESTDIR=../sysroot
+	
+}
+
+conf_uclibc(){
+	
+	install_toolchain
+	
+	prepare_uclibc
+	
+	sed -i 's|KERNEL_HEADERS=.*|KERNEL_HEADERS="../sysroot/usr/include"|g'  uclibc-ng/.config
+	rm -f build.log
+	echo "\n----------------- uClibc-ng log -------------------------- \n" > build.log
+	CROSS_COMPILE=$CONFIG_GCC_PREFIX make -C uclibc-ng menuconfig
+	
+}
 
 help(){
 	echo ""
@@ -288,16 +486,40 @@ help(){
 	echo "   --conf_kernel"
 	echo "   --upload_kernel"
 	echo "   --build_rootfs"
+	echo "   --pack_rootfs"
+	echo "   --download"
+	echo "   --run_qemu"
+	echo "   --build_uclibc"
+	echo "   --conf_uclibc"
 	echo ""
 	
 }
 
+
 export PATH=$PATH:$(pwd)/$CONFIG_TOOLCHAIN/usr/bin
 
-if [ ! -e $CONFIG_TOOLCHAIN ] ; then
-	tar -xaf ${CONFIG_TOOLCHAIN}.tar.xz
-fi
+install_toolchain(){
 
+	if [ ! -e  ${CONFIG_TOOLCHAIN}.tar.xz ] ; then
+		echo ""
+		echo -e "\033[01;31m ${CONFIG_TOOLCHAIN}.tar.xz nicht gefunden\033[00m"
+		echo ""
+		exit 1
+	fi
+
+	if [ ! -e $CONFIG_TOOLCHAIN ] ; then
+		print_status "install toolchain"
+		tar -xaf ${CONFIG_TOOLCHAIN}.tar.xz
+	fi
+
+	if [ -e $CONFIG_TOOLCHAIN/relocate-sdk.sh ] ; then
+		print_status "buildroot relocate"
+		( cd $CONFIG_TOOLCHAIN; ./relocate-sdk.sh )
+	fi
+	
+}
+
+#echo $PATH
 
 # More safety, by turning some bugs into errors.
 # Without `errexit` you don’t need ! and can replace
@@ -313,7 +535,7 @@ if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
 fi
 
 # option --output/-o requires 1 argument
-LONGOPTS=help,build_kernel,conf_kernel,upload_kernel:,build_rootfs,output:,verbose
+LONGOPTS=help,build_kernel,conf_kernel,upload_kernel:,build_rootfs,pack_rootfs,download,run_qemu,build_uclibc,conf_uclibc,output:,verbose
 OPTIONS=dfo:v
 
 # -regarding ! and PIPESTATUS see above
@@ -349,9 +571,29 @@ while true; do
             build_rootfs
             exit 0
             ;;
+        --pack_rootfs)
+			pack_rootfs
+			exit 0
+            ;;
         --upload_kernel)
             upload_kernel $2
             exit 0
+            ;;
+        --download)
+			download_dev_pack
+			exit 0
+            ;;
+        --run_qemu)
+			run_qemu
+			exit 0
+            ;;
+        --build_uclibc)
+			build_uclibc
+			exit 0
+            ;;
+        --conf_uclibc)
+			conf_uclibc
+			exit 0
             ;;
         --)
             shift
